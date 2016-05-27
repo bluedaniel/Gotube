8b5b267eaf713635ae58ee0068f5bfac08715217
bluedaniel <daniellevitt86@gmail.com>
2016-05-27 09:20:00 +0100

    Echoing json strings

--- command/status.go
+++ command/status.go
@@ -3,27 +3,40 @@ package command
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
+	"github.com/fatih/color"
 	"github.com/urfave/cli"
 )
 
-type Message struct {
-	Name, modeName []string
+// Tube contains ftl data
+type Tube struct {
+	Name, ModeName string
 }
 
-func getJSON(url string) *json.Decoder {
+func getJSON(url string) []byte {
 	r, err := http.Get(url)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer r.Body.Close()
-	return json.NewDecoder(r.Body)
+	rb, err := ioutil.ReadAll(r.Body)
+	return rb
 }
 
+// CmdStatus runs `tube status`
 func CmdStatus(c *cli.Context) error {
-	jsonData := getJSON("https://api.tfl.gov.uk/Line/Mode/tube")
+	jsonByte := getJSON("https://api.tfl.gov.uk/Line/Mode/tube")
 
-	fmt.Println(jsonData)
+	var arr []Tube
+	json.Unmarshal(jsonByte, &arr)
+
+	col := color.New(color.FgCyan).Add(color.Underline)
+	col.Println("Prints cyan text with an underline.")
+
+	for _, e := range arr {
+		fmt.Printf("%v line is of type %v \n", e.Name, e.ModeName)
+	}
 	return nil
 }
